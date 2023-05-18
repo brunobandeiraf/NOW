e8f2935e85624550c29f65878f4a71948a04a364
Daniel Barros <111021040+Yazuul@users.noreply.github.com>
2023-05-18 14:37:07 -0300

    a
    a

--- foodreview2/routes/restaurant.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const restaurant = express.Router();
-
-restaurant.get('/', (req, res) => {
-    res.send("Rota de Restaurantes")
-});
-
-export default restaurant;
